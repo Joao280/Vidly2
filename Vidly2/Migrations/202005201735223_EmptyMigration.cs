8f4784dc9bc53da53e53990f401b20493259d321
Joao280 <joaoc2800@gmail.com>
2020-05-20 18:48:42 +0100

    Populate GenreTypes

---
+++ Vidly2/Migrations/202005201735223_EmptyMigration.cs
@@ -0,0 +1,42 @@
+namespace Vidly2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EmptyMigration : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Movies", "GenreType_Id", "dbo.GenreTypes");
+            DropIndex("dbo.Movies", new[] { "GenreType_Id" });
+            //DropTable("dbo.Movies");
+            //DropTable("dbo.GenreTypes");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.GenreTypes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        GenreTypeId = c.Byte(nullable: false),
+                        GenreType_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateIndex("dbo.Movies", "GenreType_Id");
+            AddForeignKey("dbo.Movies", "GenreType_Id", "dbo.GenreTypes", "Id");
+        }
+    }
+}
--- Vidly2/Models/Movie.cs
+++ Vidly2/Models/Movie.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Vidly2.Models;
 
 namespace Vidly.Models
 {
@@ -9,5 +10,7 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public GenreType GenreType { get; set; }
+        public byte GenreTypeId { get; set; }
     }
 }
